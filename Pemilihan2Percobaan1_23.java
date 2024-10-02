6d9e5317bd9e51414ca68ca200f5f0c8e4a29254
qulbyazz <qulbyazzumy@gmail.com>
2024-10-02 19:40:51 +0700

    commit percobaan 1

---
+++ Pemilihan2Percobaan1_23.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Pemilihan2Percobaan1_23{
+    public static void main(String[] args) {
+        Scanner input23 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan Tahun: ");
+        tahun = input23.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            }
+            else{
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        }
+    }
+}
