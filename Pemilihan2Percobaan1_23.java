59974a3d6a24c8a8205698e3b1f84fddb32a6964
qulbyazz <qulbyazzumy@gmail.com>
2024-10-02 20:59:21 +0700

    pertanyaan nomor 4

--- Pemilihan2Percobaan1_23.java
+++ Pemilihan2Percobaan1_23.java
@@ -12,8 +12,10 @@ class Pemilihan2Percobaan1_23{
         if ((tahun % 4) == 0) {
             if((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
+            }else if((tahun % 400) == 0){
+                System.out.println("Termasuk Tahun Kabisat");
             }else{
-                System.out.println("Tahun tidak kabisat");
+                System.out.println("Tahun tidak kabisa");
             }
         } else{
             System.out.println("Bukan Tahun Kabisat");
