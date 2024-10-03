3897204dec259c222cd3e2ece048fecc62de0fc9
qulbyazz <qulbyazzumy@gmail.com>
2024-10-03 07:27:24 +0700

    commit percobaan 2 yang ke 2

--- Pemilihan2Percobaan2_23.java
+++ Pemilihan2Percobaan2_23.java
@@ -4,9 +4,9 @@ public class Pemilihan2Percobaan2_23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String member;
+        String member, metode_pembayaran;
         int jumlah_beli, harga = 0, pilihan_menu, potongan_QRIS = 1000;
-        double total_bayar, diskon = 0.1, total_bayar_QRIS;
+        double total_bayar, diskon = 0.1;
 
         System.out.println("-----------------------");
         System.out.println("==== MENU KAFE JTI ====");
@@ -22,43 +22,59 @@ public class Pemilihan2Percobaan2_23 {
         jumlah_beli = input.nextInt();
         System.out.print("Apakah punya member (y / n)? = ");
         member = input.next();
+        System.out.print("Masukkan Metode Pembayaran (Tunai / QRIS): ");
+        metode_pembayaran = input.next();
 
         System.out.println("-------------------------------------");
-        
+
         if (member.equals("y")) {
             System.out.println("Besar diskon = 10%");
             if (pilihan_menu == 1) {
                 harga = 12000;
-                System.out.println("Harga Kopi = " +harga);
-            }else if (pilihan_menu == 2) {
+                System.out.println("Harga Kopi = " + harga);
+            } else if (pilihan_menu == 2) {
                 harga = 7000;
-                System.out.println("Harga Teh = " +harga);
-            }else if (pilihan_menu == 3){
+                System.out.println("Harga Teh = " + harga);
+            } else if (pilihan_menu == 3) {
                 harga = 20000;
-                System.out.println("Harga Roti = " +harga);
+                System.out.println("Harga Roti = " + harga);
+            } else {
+                System.out.println("Masukkan Menu yang benar");
+                return;
             }
+
             total_bayar = harga - (harga * diskon) * jumlah_beli;
-            total_bayar_QRIS = total_bayar - potongan_QRIS;
-            System.out.println("Menu yang anda pilih: " +pilihan_menu);
-            System.out.println("Total Bayar: " +total_bayar);
-            System.out.println("Total dipotong QRIS: " +total_bayar_QRIS);
-        }else if (member.equals("n")) {
+            System.out.println("Menu yang anda pilih: " + pilihan_menu);
+            System.out.println("Total Bayar: " + total_bayar);
+            if (metode_pembayaran.equalsIgnoreCase("QRIS")) {
+                total_bayar -= 1000;
+                System.out.println("Total Bayar QRIS: " + total_bayar);
+            }
+
+        } else if (member.equals("n")) {
             System.out.println("Tidak Mendapatkan Diskon");
             if (pilihan_menu == 1) {
                 harga = 12000;
-                System.out.println("Harga Kopi = " +harga);
-            }else if (pilihan_menu == 2) {
+                System.out.println("Harga Kopi = " + harga);
+            } else if (pilihan_menu == 2) {
                 harga = 7000;
-                System.out.println("Harga Teh = " +harga);
-            }else if (pilihan_menu == 3){
+                System.out.println("Harga Teh = " + harga);
+            } else if (pilihan_menu == 3) {
                 harga = 20000;
-                System.out.println("Harga Roti = " +harga);
+                System.out.println("Harga Roti = " + harga);
+            } else {
+                System.out.println("Masukkan Menu yang benar");
+                return;
             }
             total_bayar = harga * jumlah_beli;
-            total_bayar_QRIS = total_bayar - potongan_QRIS;
-            System.out.println("Menu yang anda pilih: " +pilihan_menu);
-            System.out.println("Total Bayar: " +total_bayar);
-            System.out.println("Total dipotong QRIS: " +total_bayar_QRIS);
+
+            System.out.println("Menu yang anda pilih: " + pilihan_menu);
+            System.out.println("Total Bayar: " + total_bayar);
+
+            if (metode_pembayaran.equalsIgnoreCase("QRIS")) {
+                total_bayar -= 1000;
+                System.out.println("Total Bayar QRIS: " + total_bayar);
+            }
         }
     }
 }
